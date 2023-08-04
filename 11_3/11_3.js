7c8fbd455abef57e3d05bcfc231b9b06ca99ad16
Anastasiya Sorokina <38187366+SandraSor@users.noreply.github.com>
2023-08-04 10:42:04 +0300

    Update geolocation

--- 11_3/11_3.js
+++ 11_3/11_3.js
@@ -1,4 +1,4 @@
-const message=document.querySelector('.message');
+const input_message=document.querySelector('.input_message');
 const message_area=document.querySelector('.message_area');
 const btn_send=document.querySelector('.btn_send');
 const btn_geo=document.querySelector('.btn_geo');
@@ -11,40 +11,43 @@ function addMessage(message, position='flex-end') {
         
   let chat = message_area.innerHTML;
   message_area.innerHTML = chat + element;
-    }
-
-btn_geo.addEventListener('click', ()=>{
-  if ("geolocation" in navigator) {
-  navigator.geolocation.getCurrentPosition((position) => {
-    const { coords } = position;
-    console.log(coords.latitude, coords.longitude);
-  });
 }
-})
 
-  const error = () => {
+const error = () => {
       let error = "Позиция не может быть определена" 
       addMessage(error);
   }
     
-  const success = (position) => {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
+const success = (position) => {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
     
-    let link = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-    addLink(link)
-    }
+  let link = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+  addLink(link)
+}
     
- function addLink(link) {
-    let element = `
-      <a  href='${link}'
+function addLink(link) {
+  let element = `
+    <a  href='${link}'
       target='_blank'
       style='text-decoration: none;'
-      >
-        Гео-позиция
-       </a>
-        `;
-      let chat = textWindow.innerHTML;
-      textWindow.innerHTML = chat + element;
+    >
+      Гео-позиция
+    </a>`;
+      let chat = message_area.innerHTML;
+      message_area.innerHTML = chat + element;
     };
 
+btn_geo.addEventListener('click', ()=>{
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(success, error);
+  // navigator.geolocation.getCurrentPosition((position) => {
+  //   const { coords } = position;
+  //   console.log(coords.latitude, coords.longitude);
+  // });
+} else{
+  console.log("You can't use geolocation");
+}
+})
+
+  
